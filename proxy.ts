7101b8ff07f3d286b659360b073f171036eeb75e
rurigrass <ruripg@gmail.com>
2025-11-02 20:10:25 +0100

    rename to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { updateSession } from "./lib/supabase/middleware";
 import { type NextRequest } from "next/server";
 
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   return updateSession(request);
 }
 
